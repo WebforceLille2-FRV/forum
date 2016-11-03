83935f426d4ad07b4f43a9d6f0b46aa6c649b69e
Etudiant <Etudiant>
2016-11-03 14:47:52 +0100

    register

---
+++ private/register.php
@@ -0,0 +1,30 @@
+<?php 
+
+include 'database.php'; 
+var_dump($db);
+function register(){
+global $db;
+	if(isset($_POST['submit']) && !empty($_POST)){
+		// On nettoie les entrées utilisateur
+		$email     = trim(strip_tags($_POST['email']));
+		$email     = filter_var($email,FILTER_VALIDATE_EMAIL);
+		$emailHash = password_hash($email,PASSWORD_DEFAULT);
+		$password  = trim(strip_tags($_POST['password'])); 
+
+		$p = $db->prepare("INSERT INTO users(username,password,email,role) 
+						   VALUES(:username,:password,:email,:role)");
+	$p->bindValue(":username",$_POST['username'],PDO::PARAM_STR);
+	$p->bindValue(":password",$password ,PDO::PARAM_STR);
+	$p->bindValue("email"    ,$emailHash    ,PDO::PARAM_STR);
+	$p->bindValue(":role"    ,"user"    ,PDO::PARAM_STR);
+	
+		if($p->execute()){
+			header('Location:../public/index.php');
+			echo 'inscription validée';
+		} // Fermeture If
+	} // Fermeture isset Submit et !empty Post
+}
+
+register();
+
+?>
