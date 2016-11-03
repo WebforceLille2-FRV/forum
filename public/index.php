be9c73dfcf16e25a04e95a6dc9e2dba30bff3b57
doncodie <don.codie@hotmail.fr>
2016-11-03 01:02:49 +0100

    ommit Robin html et css v1.0.0

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style_forum.css">
+	<title>Forum</title>
+</head>
+<body>
+	<header>
+		<div class="block01">
+		</div>
+		<div class="block02">
+			<h1> Forum For Real V...</h1>
+			<h3>Slogan</h3>
+		</div>
+		<div class="block03"></div>
+	</header>
+
+	<nav>
+		<ul>
+			<li>01</li>
+			<li>01</li>
+			<li>01</li>
+			<li>01</li>
+			<li>01</li>
+		</ul>
+	</nav>
+
+	<script src="js/jquery-3.1.1.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
