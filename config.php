576f57d5d3b1dbf5f35622b6cf48ef2bc3016994
fredericsoude <fredericsoude59000@gmail.com>
2016-11-03 09:59:42 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -5,24 +5,11 @@ define('DSN', '');
 define('DBNAME', '');
 define('LOGIN', '');
 define('MOT_DE_PASSE', '');
+
 $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
-        // on change la methode de recuperation des données (tableau associatif) par defaut
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)
-        //Afficher les erreurs MySql dans le php ou alors dans le PDO::ERRMODE_EXCEPTION
-        //Rien à voir avec la config php.ini, ce sont des erreurs MySql interpretées par PDO
-
-try {
-    $db = new PDO("mysql:host="DSN, "dbname="DBNAME, LOGIN, MOT_DE_PASSE, $options);
-            // on va chercher $options dans params.php (options pour pdo).
-            
+                 PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)
+   
+    $db = new PDO("mysql:host="DSN, "dbname="DBNAME, LOGIN, MOT_DE_PASSE, $options);                        
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die(sprintf("%s dans %s à la ligne %d : %s", get_class($e), $e->getFile(), $e->getLine(), $e->getMessage()));
-}
-    if ($db) {
-        echo "Connexion à la base de données : OK"."<br />", PHP_EOL;
-        
-    }
-
 
 ?>
--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ CREATE TABLE `users` (
     `password` BINARY(40) NOT NULL,
     `email` VARCHAR(80) NOT NULL,
     `role` ENUM('Admin', 'Util') NOT NULL DEFAULT 'Util',
-    `token` TINYINT(1) NOT NULL DEFAULT 0,
+    `token` VARCHAR(80) NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY (`username`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
@@ -20,12 +20,11 @@ CREATE TABLE `posts` (
     `username` VARCHAR(80) NOT NULL,
     `answer` VARCHAR(80) NOT NULL,
     `date` DATE(y:m:s) NOT NULL,
-    `token` TINYINT(1) NOT NULL DEFAULT 0,
+    `token` VARCHAR(80) NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY (`username`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
-INSERT INTO `users`(`username`, `password`, `email`, `role`) VALUES('fredo', MD5('ronron', 'fredericsoude59000@gmail.com', 'admin');
 
 
 
