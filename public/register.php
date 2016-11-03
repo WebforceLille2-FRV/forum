e081cd6ffad23702f26c6562fdc1a10b85debd12
Etudiant <Etudiant>
2016-11-03 13:25:03 +0100

    foncion register

--- public/register.php
+++ public/register.php
@@ -1,20 +1,22 @@
 <?php 
 
-include 'database.php';
+include '../private/database.php';
 function register(){
-	// On nettoie les entrées utilisateur
-$email     = trim(strip_tags($_POST['email']));
-$email     = filter_var($email,FILTER_VALIDATE_EMAIL);
-$emailHash = password_hash($email,PASSWORD_DEFAULT);
-$password  = trim(strip_tags($_POST['password'])); 
-      
+
 	if(isset($_POST['submit']) && !empty($_POST)){
-			$p = $db->prepare("INSERT INTO users(username,password,email,role) 
-							   VALUES(:username,:password,:email,:role)");
+		// On nettoie les entrées utilisateur
+		$email     = trim(strip_tags($_POST['email']));
+		$email     = filter_var($email,FILTER_VALIDATE_EMAIL);
+		$emailHash = password_hash($email,PASSWORD_DEFAULT);
+		$password  = trim(strip_tags($_POST['password'])); 
+
+		$p = $db->prepare("INSERT INTO users(username,password,email,role) 
+						   VALUES(:username,:password,:email,:role)");
 	$p->bindValue(":username",$_POST['username'],PDO::PARAM_STR);
 	$p->bindValue(":password",$password ,PDO::PARAM_STR);
 	$p->bindValue("email"    ,$emailHash    ,PDO::PARAM_STR);
 	$p->bindValue(":role"    ,"user"    ,PDO::PARAM_STR);
+	
 		if($p->execute()){
 			header('Location:index.php');
 			echo 'inscription validée';
